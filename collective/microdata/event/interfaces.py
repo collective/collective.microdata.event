eb30a3e24234cda9a5fa64629a600cc52ab9661b
Luca Fabbri <luca@keul.it>
2012-07-21 18:36:03 +0200

    first commit

---
+++ collective/microdata/event/interfaces.py
@@ -0,0 +1,5 @@
+from zope.interface import Interface
+
+class IMicrodataEventLayer(Interface):
+    """Marker interface for the collective.microdata.event layer"""
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='collective.microdata.event',
+      version=version,
+      description="Add microdata (scheda.org) support to Plone events",
+      long_description=open("README.rst").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='plone microdata schema.org html5 event',
+      author='keul',
+      author_email='luca@keul.it',
+      url='https://github.com/keul/collective.microdata.event',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.microdata'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
