5490c3e88410dbd56eb8ba0f96fabc1f6db6b978
Luca Fabbri <luca@keul.it>
2012-07-22 16:16:50 +0200

    updated classifiers

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name='collective.microdata.event',
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
+        "Development Status :: 3 - Alpha",
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
         "Programming Language :: Python",
